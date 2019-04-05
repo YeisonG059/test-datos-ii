cb1d984683ac9e3d0605862bf4ff2a3f6b0b330d
Yeison Rafael Maldonado Gamero <yeisong@CAMPUSUNINORTE.LOCAL>
2019-04-05 10:44:13 -0500

    crear metodo fu

--- floydwarshall/src/floydwarshall/Floydwarshall.java
+++ floydwarshall/src/floydwarshall/Floydwarshall.java
@@ -1,21 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package floydwarshall;
 
-/**
- *
- * @author yeisong
- */
+
 public class Floydwarshall {
 
-    /**
-     * @param args the command line arguments
-     */
+    static void floyd_warshall(int grafo[][]){
+        int n = grafo.length;
+        int temp=0;
+        //craer matriz de distancia
+        int distancias[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                distancias[i][j]=j;
+            }
+        }
+        //ejecutar el metodo floay-warshall
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    temp = grafo[j][i]+grafo[i][k];
+                    if(temp<grafo[j][k]){
+                        grafo[j][k]=temp;
+                        distancias[j][k]=i;
+                    }
+                }
+            }
+        }
+        
+        System.out.println("La matriz de distancia");
+        print(distancias);
+        System.out.println(" .");
+        print(grafo);
+    }
+    
+    static void print(int m[][]){
+        
+        System.out.println("   ");
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                System.out.println(m[i][j]+" ");
+            }
+        }
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
